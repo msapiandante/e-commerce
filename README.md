a07422bc0240006a6a8d18308ba7f50a2b3f0bee
Melanie S <melanie.sapiandante@gmail.com>
2023-04-28 00:00:46 -0400

    deleted extra readme file, updated readme

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# e-commerce
+## Description
+
+I was tasked with creating the back end for an e-commerce site by modifying provided starter code. My goal was to configure a working Express.js API to use Sequelize to interact with a MySQL database. I was able to complete modifying the models folder, a readme and server.js. I was not able to complete the routes folder in time for submission. I plan to resubmit once complete. 
+
+
+## Credits
+https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide
+https://sequelize.org/docs/v6/core-concepts/validations-and-constraints/
+
+## License
+
+MIT License
+
+Copyright (c) 2023 msapiandante
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+## Tests
+Video demo link : 
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,27 +2,68 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-
+  // finds all categories and associated products
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+    try {
+      const categoryData = await Category.findAll({
+        include: [{ model: Product }]
+      });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+//finds one category by id value and associated products. 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
-});
+    try {
+      const categoryData = await Category.findByPk(req.params.id, {
+        include: [{ model: Product}]
+      });
+  
+      if (!categoryData) {
+        res.status(404).json({ message: 'No category found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
+//creates a new category 
 router.post('/', (req, res) => {
-  // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
 
+//delete category by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
-});
+    try {
+      const categoryData = await Category.destroy({
+        where: {
+          id: req.params.id
+        }
+      });
+  
+      if (!categoryData) {
+        res.status(404).json({ message: 'No category found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;
