cdcfa56a76ebed33dd76ff9a0bf4d7fb1466faf2
Melanie S <melanie.sapiandante@gmail.com>
2023-04-27 23:10:04 -0400

    finished code to index.js in models folder

--- models/index.js
+++ models/index.js
@@ -10,10 +10,30 @@ Product.belongsTo(Category, {
 
 });
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'product_tags'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'tag_products'
+}
+
+)
 
 module.exports = {
   Product,
